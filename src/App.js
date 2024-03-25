32dd387904ac58bfa7b116ebea61b6b070bcd590
Le Van Kiem <levankiemdev@gmail.com>
2024-03-25 12:55:50 +0700

    Cài đặt thư viện

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import { Routes, Route } from "react-router-dom";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/About/About.js
@@ -0,0 +1,4 @@
+const About = () => {
+  return <>About me</>;
+};
+export default About;
