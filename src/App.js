3dcbcff44c4a764d8b0c1d2ff657454642c1a30c
Le Van Kiem <levankiemdev@gmail.com>
2024-03-25 15:12:41 +0700

    Setup Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 // import About from "./components/About/About";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "./components/Header/Header";
+import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 // import { Routes, Route } from "react-router-dom";
 const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.count);
   return (
     <div className="App">
       <Header />
+
+      <div>Count: {count}</div>
+
+      <button onClick={() => dispatch(increaseCounter())}>
+        Increase Count
+      </button>
+
+      <button onClick={() => dispatch(decreaseCounter())}>
+        Decrease Count
+      </button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,18 +7,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/About/About";
 import AppRoute from "./routes/AppRoute";
+import { Provider } from "react-redux";
 
+import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppRoute />}>
-          <Route path="/about" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <>
+    <Provider store={store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AppRoute />}>
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    </Provider>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
