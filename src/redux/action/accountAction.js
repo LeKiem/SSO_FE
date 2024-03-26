2560bc64f6600b836da6e7b8ce479a72882e5e11
Le Van Kiem <levankiemdev@gmail.com>
2024-03-26 16:53:15 +0700

    Xử lý Reload lại website

--- src/redux/action/accountAction.js
+++ src/redux/action/accountAction.js
@@ -29,3 +29,24 @@ export const doLogin = (ssoToken) => {
   };
   // logic here that can dispatch actions or read state
 };
+export const doGetAccount = () => {
+  return async (dispatch, getState) => {
+    dispatch({ type: USER_LOGIN_REQUEST });
+    axios
+      .get(process.env.REACT_APP_BACKEND_GETACCOUNT, { withCredentials: true })
+      .then((res) => {
+        console.log(">>>> res", res);
+        if (res && +res.EC === 0) {
+          dispatch({ type: USER_LOGIN_SUCCESS, user: res.DT });
+        } else {
+          //   setMessage(res.EM);
+          dispatch({ type: USER_LOGIN_FAILED, error: res.EM });
+        }
+      })
+      .catch((err) => {
+        dispatch({ type: USER_LOGIN_FAILED, error: "Something worng....." });
+        console.log(">>>> error", err);
+      });
+  };
+  // logic here that can dispatch actions or read state
+};
